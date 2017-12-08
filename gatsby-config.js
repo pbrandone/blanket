6e19a7c29963c58e72ea261cc8ab00418d959b7f
Pedro Brandão <pedro@significa.pt>
2017-12-08 22:30:54 +0000

    Add dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,25 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Blanket Studio`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-prismic`,
+      options: {
+        repositoryName: `blanket`,
+        accessToken: `MC5XaXJ5WGlrQUFDTm9fZ0xY.XXvvv73vv70n77-9Xwnvv73vv73vv70TBu-_ve-_vRbvv71zU0xB77-9RFfvv73vv71b77-977-9ABPvv70`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `black`,
+        showSpinner: true,
+      },
+    },
+    // Needs to be last in array
+    `gatsby-plugin-netlify`
+  ],
 }
